da5fcfa4075be7e6cb0f1b20e8cc7e3f27e6620b
Andrea Bigagli <abigagli@gmail.com>
2013-06-10 17:52:57 +0200

    Fixed use of imported static library

---
+++ Applications/LibUser/Source/main.cpp
@@ -0,0 +1,10 @@
+#include "Math/Vector3.hpp"
+#include "doubler.h"
+
+int main(int argc, char *argv[])
+{
+    int x = doubler (7);
+    Math::Vector3f {x, 11, 13.1};
+
+    return 0;
+}
--- Applications/MathUser/Source/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Math/Vector3.hpp"
-
-int main(int argc, char *argv[])
-{
-    Math::Vector3f {10, 11, 13.1};
-    return 0;
-}
