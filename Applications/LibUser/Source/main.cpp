fc1f6165f2cb7e2719dce930c768ebe36ea76aee
Andrea Bigagli <abigagli@gmail.com>
2014-01-08 10:52:18 +0100

    Updated with templates for external library built with configure/make

--- Applications/LibUser/Source/main.cpp
+++ Applications/LibUser/Source/main.cpp
@@ -1,9 +1,14 @@
 #include "Math/Vector3.hpp"
-#include "doubler.h"
+
+#define MYSQLPP_MYSQL_HEADERS_BURIED
+#include <mysql++/mysql++.h>
+#include <mysql++/ssqls.h>
+#include <boost/serialization/strong_typedef.hpp>
+#include <boost/algorithm/string.hpp>
 
 int main(int argc, char *argv[])
 {
-    int x = doubler (7);
+    int x;
     Math::Vector3f {x, 11, 13.1};
 
     return 0;
